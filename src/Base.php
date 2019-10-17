1abbd59f726f56fcd0216419e7aa0182afe559e9
ffchen1227 <lilubin1000@126.com>
2019-10-17 17:03:57 +0800

    更新
    更新

---
+++ src/Base.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace ffchen\wechat;
+
+
+abstract class Base
+{
+    /**
+     * 微信组件
+     *
+     * @var Wechat
+     */
+    protected $wechat;
+
+    /**
+     * @param Wechat $wechat
+     */
+    public function __construct($wechat)
+    {
+        $this->wechat = $wechat;
+    }
+}
